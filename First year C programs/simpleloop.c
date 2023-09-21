915dd653f7ed5d73dad469d89fcd43e62c128f5c
Jyotiraditya Singh <77264025+jyotir07@users.noreply.github.com>
2023-09-22 01:41:29 +0530

    Add files via upload

---
+++ First year C programs/simpleloop.c
@@ -0,0 +1,18 @@
+/*code by Jyotiraditya Singh.*/
+/*Linkedin: https://www.linkedin.com/in/jyotir-aditya-028925249*/
+#include<stdio.h>
+int main()
+{
+int n1=0, mul, i;
+printf("Welcome!\n");
+
+// scanning the number 
+printf("Enter the number:");
+scanf("%d", &n1);
+// creating the loop for the 10 numbers
+
+for(i=1; 7; ){
+    printf("%d * %d = %d\n", n1, i, n1*i);
+}
+return 0;
+}
