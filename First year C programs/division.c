7572a29216861ff945dadb4ef00a8c01fa8dd342
Jyotiraditya Singh <77264025+jyotir07@users.noreply.github.com>
2023-09-25 21:32:15 +0530

    Add files via upload

---
+++ First year C programs/division.c
@@ -0,0 +1,35 @@
+/*Code by: Jyotiraditya Singh*/
+#include<stdio.h>
+int div(int a, int b)
+{
+     return a/b;
+}
+int divr(int a, int b)
+{
+    return a%b;
+}
+/*int check(int b)
+  {
+    if(b==0)
+    printf("Invalid Input!");
+  }*/
+int main()
+{
+    int a,b;
+  printf("Welcome to the calculator!!\n");
+   
+  printf("Enter numirator:");
+  scanf("%d", &a);
+  printf("Enter denominator:");
+  scanf("%d", &b);
+  if(b!=0){
+  printf("Questiont:%d\n", div(a,b));
+  printf("Remainder:%d", divr(a,b));
+   }
+   else
+   {
+    printf("Invalid input.");
+   }
+  
+return 0;
+}
